879e632f30fb1274898f2feacb3d2f82ac118e08
Artyom Kombarov <artyom.kombaroff@gmail.com>
2020-09-22 20:16:49 +0300

    finish intro section

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import '../styles/style.scss';
+
+import Menu from "./modules/menu";
+import dynamicRender from "./modules/dynamicRender";
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  const mainMenu = new Menu('.menu', {
+    trigger: '.menu__button',
+    content: '.content'
+  });
+
+  dynamicRender('.menu', mainMenu.close.bind(mainMenu));
+  window.addEventListener('resize', () => {
+    dynamicRender('.menu', mainMenu.close.bind(mainMenu));
+  })
+
+})
