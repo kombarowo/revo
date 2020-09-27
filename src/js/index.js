73cb4b53313aab0e55af04cc4c992e8705ae2731
Artyom Kombarov <artyom.kombaroff@gmail.com>
2020-09-27 18:58:41 +0300

    finish slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,19 @@
+import 'swiper/swiper-bundle.css';
 import '../styles/style.scss';
 import './polyfills/foreach';
 
 import smoothscroll from 'smoothscroll-polyfill';
 import Menu from "./modules/menu";
 import VideoModal from "./modules/videoModal";
+import Swiper, {Navigation} from 'swiper';
 import animOnScroll from "./modules/animOnScroll";
 import initAnchors from "./modules/anchors";
 import dynamicRender from "./modules/dynamicRender";
 
+Swiper.use([Navigation]);
+
 window.addEventListener('DOMContentLoaded', function () {
-  var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+  const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
   smoothscroll.polyfill();
 
   const mainMenu = new Menu('.menu', {
@@ -24,6 +28,27 @@ window.addEventListener('DOMContentLoaded', function () {
 
   const videoModal = new VideoModal('.modal--video', '.button--play', '.modal__close', 'player', isIE11);
   initAnchors();
+
+  const chooseSlider = new Swiper('.choose-slider', {
+    allowTouchMove: false,
+    wrapperClass: 'choose-slider__wrapper',
+    slideClass: 'choose-slider__slide',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 15,
+    slideActiveClass: 'choose-slider__slide--active',
+    slideNextClass: 'choose-slider__slide--next',
+    navigation: {
+      nextEl: '.choose-slider__button--next',
+      prevEl: '.choose-slider__button--prev',
+      disabledClass: 'choose-slider__button--disabled',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2
+      }
+    }
+  })
 
   setTimeout(() => {
     animOnScroll();
